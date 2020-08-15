5c74a5f4cee78fbae12219d3008bc2e71ab58de5
Crowdbotics <team@crowdbotics.com>
2020-08-15 13:43:48 +0000

    Update store

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,12 @@
 import * as types from "./constants"
+export const dogapi_get_breeds_image_random_read = () => ({
+  type: types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ
+})
+export const dogapi_get_breeds_image_random_readSucceeded = response => ({
+  type: types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_SUCCEEDED,
+  response
+})
+export const dogapi_get_breeds_image_random_readFailed = error => ({
+  type: types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_FAILED,
+  error
+})
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -2,4 +2,12 @@ import * as types from "./constants"
 
 const initialState = {}
 
-export default function apiReducer(state = initialState, action) {}
+export default function apiReducer(state = initialState, action) {
+  switch (action.type) {
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ:
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_SUCCEEDED:
+    case types.DOGAPI_GET_BREEDS_IMAGE_RANDOM_READ_FAILED:
+    default:
+      return state
+  }
+}
